6a6f99341d7b923ec3924358733fcc182e4a84c4
rasme95 <erikrassmussen0@gmail.com>
2024-11-09 00:32:52 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const createError = require("http-errors");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const logger = require("morgan");
+require("dotenv").config(); // Load environment variables
+
+const app = express();
+
+// Set up view engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Middleware
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/data", express.static(path.join(__dirname, "data")));
+
+// Import route files
+const indexRouter = require("./routes/index");
+const omRouter = require("./routes/om");
+const prosjekterRouter = require("./routes/prosjekter");
+const ferdigheterRouter = require("./routes/ferdigheter");
+const kontaktRouter = require("./routes/kontakt");
+const hjemRouter = require("./routes/hjem");
+
+// Use route files
+app.use("/", indexRouter);
+app.use("/om", omRouter);
+app.use("/prosjekter", prosjekterRouter);
+app.use("/ferdigheter", ferdigheterRouter);
+app.use("/kontakt", kontaktRouter);
+app.use("/hjem", hjemRouter);
+
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  // Set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // Render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
---
+++ public/javascripts/main.js
@@ -0,0 +1,20 @@
+// Show 'Back to Top' button when scrolled down
+const backToTopBtn = document.getElementById("backToTop");
+
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+};
+
+backToTopBtn.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
