05e45b00663e5d03eed76f88589c53a84d34d101
rasme95 <erikrassmussen0@gmail.com>
2024-11-09 00:43:09 +0100

    again

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const path = require("path");
 const logger = require("morgan");
-require("dotenv").config(); // Load environment variables
 
 const app = express();
 
